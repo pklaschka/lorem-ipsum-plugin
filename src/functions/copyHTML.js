3461d966f7e014a19d29e169ceba8548e8598b30
pklaschka <pabloklaschka@yahoo.de>
2018-08-16 18:25:36 +0200

    Fixing an error occurring when only one style range is present

--- src/functions/copyHTML.js
+++ src/functions/copyHTML.js
@@ -37,31 +37,17 @@ function nodesHTML(node, options) {
     if (node instanceof Text) {
         code += "<p>"; // Every node begins with a paragraph
         let beginIndex = 0; // index of the beginning of the text
-        node.styleRanges.forEach(range => {
-            let text = node.text.substr(beginIndex, range.length); // Only use text of current style range
+        console.log('Style Ranges: ', JSON.stringify(node.styleRanges));
 
-            text = text.split("&").join("&amp;"); // Replace all occurences
-            text = text.split("<").join("&lt;"); // Replace all occurences
-            text = text.split(">").join("&gt;"); // Replace all occurences
-            text = text.split("\n").join("</p>\n<p>"); // Replace all occurences
-
-            if (range.fontStyle === "Bold") {
-                let bold = options.bold;
-                text = bold.replace('$', text);
-            } else if (range.fontStyle === "Italic") {
-                let italics = options.italics;
-                text = italics.replace('$', text);
-            }
-
-            if (range.underline) {
-                let underline = options.underline;
-                text = underline.replace('$', text);
-            }
-
-            code += text; // Text has been converted to html and can be added to code
-
-            beginIndex += range.length;
-        });
+        if (node.styleRanges.length > 1) {
+            node.styleRanges.forEach(range => {
+                let text = node.text.substr(beginIndex, range.length); // Only use text of current style range
+                code += textsHTML(text, range, options);
+                beginIndex += range.length;
+            });
+        } else {
+            code += textsHTML(node.text, node.styleRanges[0], options);
+        }
         code += "</p>\n";
     }
     else if (node instanceof Group) {
@@ -73,6 +59,28 @@ function nodesHTML(node, options) {
         code += '</div>';
     }
     return code;
+}
+
+function textsHTML(text, range, options) {
+    text = text.split("&").join("&amp;"); // Replace all occurences
+    text = text.split("<").join("&lt;"); // Replace all occurences
+    text = text.split(">").join("&gt;"); // Replace all occurences
+    text = text.split("\n").join("</p>\n<p>"); // Replace all occurences
+
+    if (range.fontStyle === "Bold") {
+        let bold = options.bold;
+        text = bold.replace('$', text);
+    } else if (range.fontStyle === "Italic") {
+        let italics = options.italics;
+        text = italics.replace('$', text);
+    }
+
+    if (range.underline) {
+        let underline = options.underline;
+        text = underline.replace('$', text);
+    }
+
+    return text;
 }
 
 module.exports = copyHTML;
