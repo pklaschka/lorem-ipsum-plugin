dbe9c3787da9f3a699158e1400858f059a894d2a
pklaschka <pabloklaschka@yahoo.de>
2018-08-16 17:54:34 +0200

    Implementing `copyHTML`

--- src/functions/copyHTML.js
+++ src/functions/copyHTML.js
@@ -2,29 +2,78 @@
  * Copyright (c) 2018. by Pablo Klaschka
  */
 
-const {Text} = require("scenegraph");
+const {Text, Group, SceneNode} = require("scenegraph");
+const clipboard = require("clipboard");
 
 /**
  * Copies HTML code for a selection
  * @param {Selection} selection
+ * @param {*} options
+ * @param {string} options.bold The bold tag template (the bold text gents inserted in the `$` part of the template
+ * @param {string} options.italics The italics tag template (the italic text gents inserted in the `$` part of the template
+ * @param {boolean} options.layerNameComments Wheter to include layer name comments
  */
-function copyHTML(selection) {
-    for (let node of selection.items) {
-        if (node instanceof Text && node.areaBox) {
-            if (node.clippedByArea) {
-                // Need to increase the height
-                while (node.clippedByArea) {
-                    node.resize(node.localBounds.width, node.localBounds.height + 1);
-                }
-            } else {
-                // Need to decrease the height
-                while (!node.clippedByArea) {
-                    node.resize(node.localBounds.width, node.localBounds.height - 1);
-                }
-                node.resize(node.localBounds.width, node.localBounds.height + 1);
+function copyHTML(selection, options) {
+    let code = "";
+
+    selection.items.forEach(node => {
+        code += nodesHTML(node, options);
+    });
+    console.log(code);
+    clipboard.copyText(code);
+}
+
+/**
+ * Recursive function to retrieve elements HTML
+ * @param {SceneNode} node
+ * @param {*} options
+ * @param {string} options.bold The bold tag template (the bold text gents inserted in the `$` part of the template
+ * @param {string} options.italics The italics tag template (the italic text gents inserted in the `$` part of the template
+ * @param {string} options.underline The underline tag template (the underline text gents inserted in the `$` part of the template
+ * @param {boolean} options.layerNameComments Wheter to include layer name comments
+ */
+function nodesHTML(node, options) {
+    let code = "<!-- " + node.name + " -->\n"; // Prefix node name (useful when multiple elements get selected)
+    if (node instanceof Text) {
+        code += "<p>"; // Every node begins with a paragraph
+        let beginIndex = 0; // index of the beginning of the text
+        node.styleRanges.forEach(range => {
+            let text = node.text.substr(beginIndex, range.length); // Only use text of current style range
+
+            text = text.split("&").join("&amp;"); // Replace all occurences
+            text = text.split("<").join("&lt;"); // Replace all occurences
+            text = text.split(">").join("&gt;"); // Replace all occurences
+            text = text.split("\n").join("</p>\n<p>"); // Replace all occurences
+
+            if (range.fontStyle === "Bold") {
+                let bold = options.bold;
+                text = bold.replace('$', text);
+            } else if (range.fontStyle === "Italic") {
+                let italics = options.italics;
+                text = italics.replace('$', text);
             }
-        }
+
+            if (range.underline) {
+                let underline = options.underline;
+                text = underline.replace('$', text);
+            }
+
+            code += text; // Text has been converted to html and can be added to code
+
+            beginIndex += range.length;
+        });
+        code += "</p>\n";
     }
+    else if (node instanceof Group) {
+        let nodeClass = node.name.replace(/\W/g, '_');
+        code += `<div class="${nodeClass}">`;
+        node.children.forEach(sceneNode =>
+            code += nodesHTML(sceneNode, options)
+        );
+        code += '</div>';
+    }
+    code += "<!-- " + node.name + " -->\n"; // Prefix node name (useful when multiple elements get selected)
+    return code;
 }
 
 module.exports = copyHTML;
--- src/modals/copyHTMLModal.js
+++ src/modals/copyHTMLModal.js
@@ -2,14 +2,16 @@
  * Copyright (c) 2018. by Pablo Klaschka
  */
 
+const storage = require('../helpers/storage');
+
 /**
  * @param {Selection} selection
  */
 async function showModal(selection) {
     console.log('Showing TAT main modal');
     let options = await modalAsync(selection);
-    /*const lorem = require('../functions/lorem');
-    await lorem(selection, options);*/
+    const copyHTML = require('../functions/copyHTML');
+    await copyHTML(selection, options);
     return true;
 }
 
@@ -18,12 +20,19 @@ async function showModal(selection) {
  */
 async function modalAsync(selection) {
     return new Promise((resolve, reject) => {
-        // Removing old instances
-        document.body.innerHTML = '';
+        storage.get('copyHTMLOptions', {
+            layerNameComments: true,
+            bold: '<strong>$</strong>',
+            italics: '<i>$</i>',
+            underline: '<a href="#">$</a>'
+        }).then(uiOptions => {
 
-        const dialog = document.createElement('dialog');
-        dialog.id = 'loremModal';
-        dialog.innerHTML = `
+            // Removing old instances
+            document.body.innerHTML = '';
+
+            const dialog = document.createElement('dialog');
+            dialog.id = 'loremModal';
+            dialog.innerHTML = `
     <style>    
     form {
         min-width: 360px;
@@ -31,57 +40,76 @@ async function modalAsync(selection) {
     </style>
     `;
 
-        const form = document.createElement('form');
-        form.method = 'dialog';
+            const form = document.createElement('form');
+            form.method = 'dialog';
 
-        const heading = document.createElement('h1');
-        heading.innerHTML = 'Copy HTML';
-        form.appendChild(heading);
+            const heading = document.createElement('h1');
+            heading.innerHTML = 'Copy HTML';
+            form.appendChild(heading);
 
-        const description = document.createElement('p');
-        description.innerHTML = `Copies markup for the selected nodes to clipboard as HTML. When multiple nodes are selected, the order the nodes were selected in determines the order in the HTML.
+            const description = document.createElement('p');
+            description.innerHTML = `Copies markup for the selected nodes to clipboard as HTML. When multiple nodes are selected, the order the nodes were selected in determines the order in the HTML.
         `;
-        form.appendChild(description);
+            form.appendChild(description);
 
-        form.appendChild(checkBox('Include layer name comments', true));
-        form.appendChild(checkBox('Sorround with layer name tags', false));
-        form.appendChild(selectBox('Bold-Tag', [
-            {value: 'strong', label: '&lt;strong&gt;[…]&lt;/strong&gt;'},
-            {value: 'b', label: '&lt;b&gt;[…]&lt;/b&gt;'},
-            {value: 'em', label: '&lt;em&gt;[…]&lt;/em&gt;'},
-        ], 'strong'));
-        form.appendChild(selectBox('Italics-Tag', [
-            {value: 'i', label: '&lt;i&gt;[…]&lt;/strong&gt;'},
-            {value: 'em', label: '&lt;em&gt;[…]&lt;/em&gt;'},
-        ], 'i'));
+            const layerNameComments = checkBox('Include layer name comments', uiOptions.layerNameComments);
+            form.appendChild(layerNameComments);
 
-        const footer = document.createElement('footer');
-        const btnOk = document.createElement('button');
-        btnOk.id = "ok";
-        btnOk.type = "submit";
-        btnOk.innerHTML = 'Copy HTML to clipboard';
-        btnOk.setAttribute('uxp-variant', 'cta');
-        btnOk.onclick = () => {
-            console.log("Lorem Ipsum");
-            dialog.close();
-            resolve();
-            document.body.innerHTML = '';
-        };
-        const btnCancel = document.createElement('button');
-        btnCancel.id = "cancel";
-        btnCancel.innerHTML = 'Cancel';
-        btnCancel.onclick = () => {
-            console.log("Closing Text Area Tools");
-            dialog.close();
-            reject();
-            document.body.innerHTML = '';
-        };
-        footer.appendChild(btnCancel);
-        footer.appendChild(btnOk);
-        form.appendChild(footer);
-        dialog.appendChild(form);
-        document.body.appendChild(dialog);
-        dialog.showModal().then(() => resolve()).catch(() => reject());
+            // form.appendChild(checkBox('Sorround with layer name tags', false));
+            const bold = selectBox('Bold-Tag', [
+                {value: '<strong>$</strong>', label: '&lt;strong&gt;[…]&lt;/strong&gt;'},
+                {value: '<b>$</b>', label: '&lt;b&gt;[…]&lt;/b&gt;'},
+                {value: '<em>$</em>', label: '&lt;em&gt;[…]&lt;/em&gt;'},
+            ], uiOptions.bold);
+            form.appendChild(bold);
+
+            const italics = selectBox('Italics-Tag', [
+                {value: '<i>$</i>', label: '&lt;i&gt;[…]&lt;/strong&gt;'},
+                {value: '<em>$</em>', label: '&lt;em&gt;[…]&lt;/em&gt;'},
+            ], uiOptions.italics);
+            form.appendChild(italics);
+
+            const underline = selectBox('Underline-Tag', [
+                {value: '<a href="#">$</a>', label: '&lt;a href="#"&gt;[…]&lt;/a&gt;'},
+                {value: '<u>$</u>', label: '&lt;u&gt;[…]&lt;/u&gt;'},
+            ], uiOptions.underline);
+            form.appendChild(underline);
+
+            const footer = document.createElement('footer');
+            const btnOk = document.createElement('button');
+            btnOk.id = "ok";
+            btnOk.type = "submit";
+            btnOk.innerHTML = 'Copy HTML to clipboard';
+            btnOk.setAttribute('uxp-variant', 'cta');
+            btnOk.onclick = () => {
+                const options = {
+                    layerNameComments: layerNameComments.childNodes.item(0).checked,
+                    bold: bold.childNodes.item(1).value,
+                    italics: italics.childNodes.item(1).value,
+                    underline: underline.childNodes.item(1).value,
+                };
+                storage.set('copyHTMLOptions', options);
+                console.log("Lorem Ipsum");
+                dialog.close();
+                resolve(options);
+                document.body.innerHTML = '';
+            };
+            const btnCancel = document.createElement('button');
+            btnCancel.id = "cancel";
+            btnCancel.innerHTML = 'Cancel';
+            btnCancel.onclick = () => {
+                console.log("Closing Text Area Tools");
+                dialog.close();
+                reject();
+                document.body.innerHTML = '';
+            };
+            footer.appendChild(btnCancel);
+            footer.appendChild(btnOk);
+            form.appendChild(footer);
+            dialog.appendChild(form);
+            document.body.appendChild(dialog);
+            dialog.showModal().then(() => resolve()).catch(() => reject());
+        });
     });
 }
 
