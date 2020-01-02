3379694b7375968bcca0d751dc92add80b2631bd
Pablo Klaschka <contact@pabloklaschka.de>
2020-01-02 16:43:57 +0100

    Added more tests

---
+++ tests/001_localization.test.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020. by Pablo Klaschka
+ */
+const fs = require('fs');
+const path = require('path');
+
+describe('Localization', () => {
+    it('should contain default translations', () => {
+        const exists = fs.existsSync(path.join(__dirname, '../dist/lang/default.json'));
+
+        expect(exists).toBeTruthy();
+    });
+
+    it('should have all language keys in all translations', () => {
+        const contents = fs.readdirSync(path.join(__dirname, '../dist/lang/')).filter(path => path.endsWith('.json')).map(fileName => {
+            return JSON.parse(fs.readFileSync(path.join(__dirname, '../dist/lang/', fileName)).toString());
+        });
+
+        for (let content of contents) {
+            expect(Object.keys(content)).toStrictEqual(Object.keys(contents[0]));
+        }
+    });
+});
--- tests/010_sample.test.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2020. by Pablo Klaschka
- */
-
-describe('Plugin', () => {
-    it('should pass the sample test', () => {
-        expect('a').toBe('A'.toLowerCase());
-    });
-});
